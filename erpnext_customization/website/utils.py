95e001ccdaaaa320b499b8e769885b526d636474
britlog <info@britlog.com>
2018-02-05 22:28:45 +0100

    Payment logo in footer

--- erpnext_customization/website/utils.py
+++ erpnext_customization/website/utils.py
@@ -10,3 +10,4 @@ import frappe.defaults
 def update_website_context(context):
 	context["footer_company_name"] = frappe.db.get_value("Website Settings", "Website Settings", "footer_company_name")
 	context["facebook_link"] = frappe.db.get_value("Website Settings", "Website Settings", "facebook_link")
+	context["enabled_checkout"] = frappe.get_doc("Shopping Cart Settings").enable_checkout
