fe87abf90834d938deb5673328a89f779aa46978
britlog <info@britlog.com>
2018-07-16 21:58:02 +0200

    [Fix] Number spinner

--- erpnext_customization/public/js/shopping_cart.js
+++ erpnext_customization/public/js/shopping_cart.js
@@ -5,20 +5,6 @@
 frappe.provide("erpnext.shopping_cart");
 var shopping_cart = erpnext.shopping_cart;
 
-frappe.ready(function() {
-	var full_name = frappe.session && frappe.session.user_fullname;
-	// update user
-	if(full_name) {
-		$('.navbar li[data-label="User"] a')
-			.html('<i class="fa fa-fixed-width fa fa-user"></i> ' + full_name);
-	}
-
-	// update login
-	shopping_cart.show_shoppingcart_dropdown();
-	shopping_cart.set_cart_count();
-	shopping_cart.bind_dropdown_cart_buttons();
-});
-
 $.extend(shopping_cart, {
 	show_shoppingcart_dropdown: function() {
 		if(frappe.session.user==="Guest") {
@@ -34,34 +20,6 @@ $.extend(shopping_cart, {
 							}
 						}
 					});
-				}
-			});
-		}
-	},
-
-	update_cart: function(opts) {
-		if(frappe.session.user==="Guest") {
-			if(localStorage) {
-				localStorage.setItem("last_visited", window.location.pathname);
-			}
-			window.location.href = "/login";
-		} else {
-			return frappe.call({
-				type: "POST",
-				method: "erpnext.shopping_cart.cart.update_cart",
-				args: {
-					item_code: opts.item_code,
-					qty: opts.qty,
-					with_items: opts.with_items || 0
-				},
-				btn: opts.btn,
-				callback: function(r) {
-					shopping_cart.set_cart_count();
-					if (r.message.shopping_cart_menu) {
-						$('.shopping-cart-menu').html(r.message.shopping_cart_menu);
-					}
-					if(opts.callback)
-						opts.callback(r);
 				}
 			});
 		}
@@ -95,49 +53,6 @@ $.extend(shopping_cart, {
 		} else {
 			$badge.html(0);
 		}
-	},
-
-	shopping_cart_update: function(item_code, newVal, cart_dropdown) {
-		frappe.freeze();
-		shopping_cart.update_cart({
-			item_code: item_code,
-			qty: newVal,
-			with_items: 1,
-			btn: this,
-			callback: function(r) {
-				frappe.unfreeze();
-				if(!r.exc) {
-					$(".cart-items").html(r.message.items);
-					$(".cart-tax-items").html(r.message.taxes);
-					if (cart_dropdown != true) {
-						$(".cart-icon").hide();
-					}
-				}
-			},
-		});
-	},
-
-
-	bind_dropdown_cart_buttons: function () {
-		$(".cart-icon").on('click', '.number-spinner button', function () {
-			var btn = $(this),
-				input = btn.closest('.number-spinner').find('input'),
-				oldValue = input.val().trim(),
-				newVal = 0;
-
-			if (btn.attr('data-dir') == 'up') {
-				newVal = parseInt(oldValue) + 1;
-			} else {
-				if (oldValue > 1) {
-					newVal = parseInt(oldValue) - 1;
-				}
-			}
-			input.val(newVal);
-			var item_code = input.attr("data-item-code");
-			shopping_cart.shopping_cart_update(item_code, newVal, true);
-			return false;
-		});
-
 	},
 
 });
