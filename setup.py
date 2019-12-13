d74ef95031f27951681bfa654f3416c73d38bb2e
britlog <info@britlog.com>
2019-12-13 23:05:18 +0100

    V11 migration

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+	from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+	from pip.req import parse_requirements
+import re, ast
 
 version = '0.0.1'
 requirements = parse_requirements("requirements.txt", session="")
