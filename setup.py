96b3a561957a4d506f4765ff7bfc2047879e17c7
britlog <info@britlog.com>
2020-05-26 23:46:29 +0200

    fix setup.py requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from setuptools import setup, find_packages
-try: # for pip >= 10
-	from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-	from pip.req import parse_requirements
 import re, ast
 
-version = '0.0.1'
-requirements = parse_requirements("requirements.txt", session="")
+# get version from __version__ variable in erpnext_customization/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+with open('erpnext_customization/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='erpnext_customization',
@@ -18,6 +22,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
