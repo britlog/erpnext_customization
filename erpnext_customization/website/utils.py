43e9ee2f144c555c4810da3c217254086cc3f19a
britlog <info@britlog.com>
2021-09-29 19:36:00 +0200

    v13 migration

--- erpnext_customization/website/utils.py
+++ erpnext_customization/website/utils.py
@@ -10,5 +10,5 @@ import frappe.defaults
 def update_website_context(context):
 	context["footer_company_name"] = frappe.db.get_value("Website Settings", "Website Settings", "footer_company_name")
 	context["facebook_link"] = frappe.db.get_value("Website Settings", "Website Settings", "facebook_link")
-	context["enabled_checkout"] = frappe.get_doc("Shopping Cart Settings").enable_checkout
+	context["enabled_checkout"] = frappe.db.get_value("E Commerce Settings", "E Commerce Settings", "enable_checkout")
 	context["shopping_cart_enabled"] = frappe.db.get_singles_value("Shopping Cart Settings", "enabled")
